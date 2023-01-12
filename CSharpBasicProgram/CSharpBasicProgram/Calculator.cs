3eeb1fe41c13ac3ba8140e7a4db76553fa130fd7
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-12 18:23:50 +0530

    [Prashant] Adding Addition functionality

---
+++ CSharpBasicProgram/CSharpBasicProgram/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasicProgram
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// Instance Variables
+        /// </summary>
+        public int first, second, sum;
+        /// <summary>
+        /// Addition of 2 numbers to get sum
+        /// </summary>
+        public Calculator(int firstNumber, int secondNumer)
+        {
+            first = firstNumber;
+            second = secondNumer;
+        }
+       
+        public void Addition()
+        {
+            sum = first + second;
+            Console.WriteLine("sum:" + sum);
+        }
+           
+
+    }
+}
---
+++ CSharpBasicProgram/CSharpBasicProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CSharpBasicProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to CSharp Program");
+            Console.WriteLine("Please enter the First Number: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the Second Number: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Calculator calculator = new Calculator(first, second);
+            calculator.Addition();
+            Console.ReadLine();
+        }
+    }
+}
