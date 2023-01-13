97bd4d197bddb9966e6cd0464ee8b9e09ae4769c
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-13 16:18:33 +0530

    [Prashant] Adding Weekday using Switch case

--- CSharpBasicProgram/CSharpBasicProgram/Program.cs
+++ CSharpBasicProgram/CSharpBasicProgram/Program.cs
@@ -7,25 +7,35 @@ namespace CSharpBasicProgram
         {
             Console.WriteLine("Welcome to CSharp Program");
             Console.WriteLine("Choose any options to perform");
-            Console.WriteLine("1:Calculator");
+            Console.WriteLine("1:Calculator\n2:SwitchCase\n10:Exit");
             int options = Convert.ToInt32(Console.ReadLine());
             switch(options)
             {
                 case 1:
+                    Console.WriteLine("Please enter the First Number: ");
+                    int first = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter the Second Number: ");
+                    int second = Convert.ToInt32(Console.ReadLine());
+                    Calculator calculator = new Calculator(first, second);
+                    calculator.Addition();
+                    calculator.Subtraction();
                     break;
+                case 2:
+                    Console.WriteLine("Please choose a day number");
+                    int day = Convert.ToInt32(Console.ReadLine());
+                    WeekDay.FindDaysOfWeeks(day);
+                    break;
+                case 10:   
                 default:
+                    Console.WriteLine("Please choose number between 1 to 7");
                     break;
 
             }
+            Console.ReadLine() ;
             
-            Console.WriteLine("Please enter the First Number: ");
-            int first = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter the Second Number: ");
-            int second = Convert.ToInt32(Console.ReadLine());
-            Calculator calculator = new Calculator(first, second);
-            calculator.Addition();
-            calculator.Subtraction();
-            Console.ReadLine();
+           
+
+            
         }
     }
 }
---
+++ CSharpBasicProgram/CSharpBasicProgram/WeekDay.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasicProgram
+{
+    public class WeekDay
+    {
+        public static void FindDaysOfWeeks(int dayNumber)
+        {
+            switch(dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("It is a Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("It is a Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("It is a Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("It is a Thrusday");
+                    break;
+                case 5:
+                    Console.WriteLine("It is a Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("It is a Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("It is a Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Please choose number between 1 to 7 ");
+                    break;
+
+            }
+        }
+    }
+}
